b84230800ecd9cb8ecfa34cd432fbad06783855b
Park Sejun <qnpfrdl1@gmail.com>
2019-01-08 16:51:50 +0900

    다시

--- Assets/Scripts/Game/GameInstance.cs
+++ Assets/Scripts/Game/GameInstance.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class GameInstance : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    static GameInstance _instance = null;
+
+    public static GameInstance Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType(typeof(GameInstance)) as GameInstance;
+
+                if(_instance == null)
+                {
+                    Debug.LogError("GameInstance dosen't exist");
+                }
+            }
+
+            return _instance;
+        }
+    }
+
 }
---
+++ Assets/Scripts/Game/GameplayStatics.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayStatics : MonoBehaviour {
+
+    static GameController _gameController;
+    static PlayerController _playerController;
+    static SceneController _sceneController;
+    static GameState _gameState;
+    static PlayerState _playerState;
+
+    public static GameController gameController
+    {
+        get
+        {
+            if (_gameController == null)
+            {
+                _gameController = FindObjectOfType(typeof(GameController)) as GameController;
+
+                if (_gameController == null)
+                {
+                    Debug.LogError("GameController dosen't exist");
+                }
+            }
+            return _gameController;
+        }
+    }
+}
